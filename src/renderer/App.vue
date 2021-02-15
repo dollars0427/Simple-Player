aaa7fcc5a6e2c9472ab0fb2d0ecc82a874df1b46
Sardo Ip <sardo.ip@sardo.work>
2021-02-15 19:04:23 +0800

    Can load local video file now.

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -5,11 +5,34 @@
 </template>
 
 <script>
-  export default {
-    name: 'simple-player'
-  }
+export default {
+  name: 'simple-player'
+}
 </script>
 
-<style>
-  /* CSS */
+<style lang="scss" global>
+.btn{
+  display: inline-block;
+  font-weight: 400;
+  text-align: center;
+  white-space: nowrap;
+  vertical-align: middle;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  border: 1px solid transparent;
+  border-top-color: transparent;
+  border-right-color: transparent;
+  border-bottom-color: transparent;
+  border-left-color: transparent;
+  padding: .375rem .75rem;
+  font-size: 1rem;
+  line-height: 1.5;
+  border-radius: .25rem;
+  transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+  color: #fff;
+  background-color: #007bff;
+  border-color: #007bff;
+}
 </style>
--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -2,127 +2,100 @@
   <div id="wrapper">
     <img id="logo" src="~@/assets/logo.png" alt="electron-vue">
     <main>
-      <div class="left-side">
-        <span class="title">
-          Welcome to your new project!
-        </span>
-        <system-information></system-information>
-      </div>
-
-      <div class="right-side">
-        <div class="doc">
-          <div class="title">Getting Started</div>
-          <p>
-            electron-vue comes packed with detailed documentation that covers everything from
-            internal configurations, using the project structure, building your application,
-            and so much more.
-          </p>
-          <button @click="open('https://simulatedgreg.gitbooks.io/electron-vue/content/')">Read the Docs</button><br><br>
-        </div>
-        <div class="doc">
-          <div class="title alt">Other Documentation</div>
-          <button class="alt" @click="open('https://electron.atom.io/docs/')">Electron</button>
-          <button class="alt" @click="open('https://vuejs.org/v2/guide/')">Vue.js</button>
+      <div class="software-info">
+        <h1 class="title">
+          Welcome to simple video player.
+        </h1>
+        <div  class="slogan">
+          Just select file, then play it.
         </div>
       </div>
-    </main>
-  </div>
+      <div class="video-test">
+        <video
+        id="video-player"
+        class="video-js"
+        controls
+        preload="auto"
+        data-setup="{}"
+        ref="player"
+        >
+      </video>
+    </div>
+    <div class="button-wrapper">
+      <button class="btn" v-on:click="selectFile">Open File</button>
+    </div>
+  </main>
+</div>
 </template>
 
 <script>
-  import SystemInformation from './LandingPage/SystemInformation'
-
-  export default {
-    name: 'landing-page',
-    components: { SystemInformation },
-    methods: {
-      open (link) {
-        this.$electron.shell.openExternal(link)
+import videojs from 'video.js';
+import 'video.js/dist/video-js.css';
+export default {
+  name: 'Home',
+  methods: {
+    async selectFile(){
+      const dialog = this.$electron.remote.dialog;
+      const files = dialog.showOpenDialog({
+        properties: ['openFile'],
+        filters: {
+          "name": "video",
+          "extensions": ['mp4'],
+        },
+      });
+      const file = files[0];
+      if(file){
+        const video = `file:///${file}`;
+        const videoPlayerEle = this.$refs.player;
+        const videoPlayer = videojs(videoPlayerEle);
+        videoPlayer.src(video);
       }
-    }
-  }
+    },
+  },
+}
 </script>
 
-<style>
-  @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
 
-  * {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body { font-family: 'Source Sans Pro', sans-serif; }
+
+#wrapper {
+  background:
+  radial-gradient(
+  ellipse at top left,
+  rgba(255, 255, 255, 1) 40%,
+  rgba(229, 229, 229, .9) 100%
+  );
+  height: 100vh;
+  padding: 60px 80px;
+  width: 100vw;
+}
+
+#logo {
+  height: auto;
+  margin-bottom: 20px;
+  width: 25%;
+  margin-left: auto;
+  margin-right: auto;
+  display: block;
+}
+
+.software-info{
+  text-align: center;
+  .slogan{
+    font-size: 1.1em;
+    margin-top: 20px;
   }
-
-  body { font-family: 'Source Sans Pro', sans-serif; }
-
-  #wrapper {
-    background:
-      radial-gradient(
-        ellipse at top left,
-        rgba(255, 255, 255, 1) 40%,
-        rgba(229, 229, 229, .9) 100%
-      );
-    height: 100vh;
-    padding: 60px 80px;
-    width: 100vw;
-  }
-
-  #logo {
-    height: auto;
-    margin-bottom: 20px;
-    width: 420px;
-  }
-
-  main {
-    display: flex;
-    justify-content: space-between;
-  }
-
-  main > div { flex-basis: 50%; }
-
-  .left-side {
-    display: flex;
-    flex-direction: column;
-  }
-
-  .welcome {
-    color: #555;
-    font-size: 23px;
-    margin-bottom: 10px;
-  }
-
-  .title {
-    color: #2c3e50;
-    font-size: 20px;
-    font-weight: bold;
-    margin-bottom: 6px;
-  }
-
-  .title.alt {
-    font-size: 18px;
-    margin-bottom: 10px;
-  }
-
-  .doc p {
-    color: black;
-    margin-bottom: 10px;
-  }
-
-  .doc button {
-    font-size: .8em;
-    cursor: pointer;
-    outline: none;
-    padding: 0.75em 2em;
-    border-radius: 2em;
-    display: inline-block;
-    color: #fff;
-    background-color: #4fc08d;
-    transition: all 0.15s ease;
-    box-sizing: border-box;
-    border: 1px solid #4fc08d;
-  }
-
-  .doc button.alt {
-    color: #42b983;
-    background-color: transparent;
-  }
+}
+.button-wrapper{
+  text-align: center;
+  margin-top:  30px;
+}
 </style>
